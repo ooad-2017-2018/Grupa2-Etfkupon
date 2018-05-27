da304161b1400f4572e0ef2d03ee07102b328230
abrdanin <b.adnan33@gmail.com>
2018-05-27 18:33:51 +0200

    dodana pattern diskusija (docx dokument)

---
+++ Projekt/ETFKupon/ETFKupon/Model/proba.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETFKupon.Model
+{
+    public class proba
+    {
+        public string id; 
+        public string tekst;
+        public proba() { id = "0"; tekst = ""; }
+    }
+}
--- Projekt/ETFKupon/ETFKupon/ModelView/KupacModelView.cs
+++ Projekt/ETFKupon/ETFKupon/ModelView/KupacModelView.cs
@@ -31,6 +31,7 @@ namespace ETFKupon.ModelView
         public void dodaj(object parametar)
         {
             IMobileServiceTable<Kupac> userTableObj = App.MobileService.GetTable<Kupac>();
+            IMobileServiceTable<proba> blabla = App.MobileService.GetTable<proba>();
             Validacija vKupac = new Validacija();
             Tuple<int, string> vAdresa;
             Tuple<int, string> vIme;
@@ -50,8 +51,11 @@ namespace ETFKupon.ModelView
             int suma = vAdresa.Item1 + vIme.Item1 + vPrezime.Item1 + vPassword.Item1 + vEmail.Item1 + vUsername.Item1 + vPasswordPotvrda.Item1;
             if (suma == 0)
             {
+                proba x = new proba();
+                x.tekst = "tekst za bazu";
                 MainPage.etfKupon.ListaKupaca.Add(Kupac);
                 userTableObj.InsertAsync(Kupac);
+                blabla.InsertAsync(x);
                 NavigationService.Navigate(typeof(MainPage), new MainPage(this));
                 return;
             }
