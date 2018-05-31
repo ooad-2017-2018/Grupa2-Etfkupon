2c06f54ef768c6eecee233aa7b24d6594d67368a
abrdanin <b.adnan33@gmail.com>
2018-05-31 18:44:47 +0200

    dodano odredjivanje role i password provjera na login pageu

--- Projekt/ETFKuponASPNET/ETFKupon/ETFKupon/Controllers/HomeController.cs
+++ Projekt/ETFKuponASPNET/ETFKupon/ETFKupon/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ETFKupon.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -26,9 +27,55 @@ namespace ETFKupon.Controllers
 
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Index(string usernameLogin, string passwordLogin)
+        {
+            DatabaseContext db = new DatabaseContext();
+            List<ETFKupon.Models.KupacBaza> listaKupaca = new List<Models.KupacBaza>();
+            List<ETFKupon.Models.FirmaBaza> listaFirmi = new List<Models.FirmaBaza>();
+            listaKupaca = db.KupacBaza.ToList();
+            listaFirmi = db.FirmaBaza.ToList();
+            for (int i = 0; i < listaKupaca.Count; i++)
+            {
+                if (listaKupaca[i].Username.Equals(usernameLogin))
+                {
+
+                    if (passwordLogin.Equals(listaKupaca[i].Password))
+                        return RedirectToAction("Index", "KupacBaza");
+                    else
+                    {
+                        //ModelState.AddModelError("Password", "Password ne valja!");
+                        return View();
+                    }
+
+                }
+            }
+            for (int i = 0; i < listaFirmi.Count; i++)
+            {
+                if (listaFirmi[i].Username.Equals(usernameLogin))
+                {
+                    if (passwordLogin.Equals(listaFirmi[i].Password))
+                        return RedirectToAction("Index", "FirmaBaza");
+                    else
+                    {
+                        //ModelState.AddModelError("Password", "Password ne valja!");
+                        return View();
+                    }
+                }
+            }
+            return Content($"E tebe nema {usernameLogin} {passwordLogin}");
+
+        }
         /*public ActionResult Login()
         {
             return View("KupacBaza");
         }*/
     }
 }
+/*
+ @using (Html.BeginForm("Index", "KupacBaza"))
+    {
+        <p><button type="submit" class="btn btn-primary btn-lg">Login</button></p>
+    }
+*/
